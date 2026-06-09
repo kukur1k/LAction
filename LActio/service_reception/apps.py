f651cae5d106f22faa300c0a5929c5f27963c090
Pyrkin <apurkin97@gmail.com>
2026-06-09 10:28:30 +0300

    make system admin page

--- LActio/service_reception/apps.py
+++ LActio/service_reception/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
 
-class ReceptionConfig(AppConfig):
+class ServiceReceptionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'service_reception'
+    verbose_name = 'LActio'
